fa5779f8937bf3ab7c7c091d4060bff49e8da46d
Yuri Baruk <Baruk Freitas>
2025-02-18 16:23:34 -0300

    fix: timeout for api response

--- projeto-rest-aula03/src/main/java/com/vemser/rest/client/BaseClient.java
+++ projeto-rest-aula03/src/main/java/com/vemser/rest/client/BaseClient.java
@@ -2,20 +2,26 @@ package com.vemser.rest.client;
 
 import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.HttpClientConfig;
 import io.restassured.config.LogConfig;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
 public class BaseClient {
-    final String BASE_URI = "http://localhost:3000";
+    final String BASE_URI = "https://serverest.dev/";
 
     public RequestSpecification set() {
 
         return new RequestSpecBuilder()
                 .setBaseUri(BASE_URI)
                 .setContentType(ContentType.JSON)
-                .setConfig(RestAssured.config().logConfig(
-                        LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails()))
+                .setConfig(RestAssured.config()
+                        .httpClient(HttpClientConfig.httpClientConfig()
+                                .setParam("http.connection.timeout", 10000)
+                                .setParam("http.socket.timeout", 10000))
+                        .logConfig(
+                        LogConfig.logConfig()
+                                .enableLoggingOfRequestAndResponseIfValidationFails()))
                 .build()
                 ;
     }
